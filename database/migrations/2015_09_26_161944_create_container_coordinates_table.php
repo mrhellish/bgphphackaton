a82aa0da8925b3d15e41b2e617700398a4da167d
Miroslav Vitanov <mvvitanov@gmail.com>
2015-09-26 19:30:04 +0300

    Add migrations

---
+++ database/migrations/2015_09_26_161944_create_container_coordinates_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateContainerCoordinatesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('containers_coordinates', function (Blueprint $table) {
+            $table->increments('id');
+            $table->integer('container_id')->unsigned();
+            $table->float('longitude');
+            $table->float('latitude');
+            $table->timestamps();
+
+            $table->foreign('container_id')->references('id')->on('containers');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::drop('containers_coordinates');
+    }
+}
