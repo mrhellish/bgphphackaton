321e1b4016c75d05d2139e549312f4369f16f413
Miroslav Vitanov <mvvitanov@gmail.com>
2015-09-26 20:33:23 +0300

    Add Controller logic

--- app/Http/Controllers/CoordinatesController.php
+++ app/Http/Controllers/CoordinatesController.php
@@ -2,6 +2,9 @@
 
 namespace App\Http\Controllers;
 
+use Illuminate\Http\Request;
+
+use App\Container;
 use App\Coordinate;
 
 class CoordinatesController extends Controller {
@@ -11,26 +14,56 @@ class CoordinatesController extends Controller {
 		return Coordinate::all();
 	}
 
-	public function get($id)
+	public function getByContainer($name)
 	{
-		$container = Coordinate::findOrFail($id);
+		$container = Container::with(['coordinates' => function($q){
+			$q->orderBy('created_at', 'desc')->first(['longitude', 'latitude']);
+		}])->where('name', $name)->first();
 
-		return $container;
+		$return = ['success' => false];
+
+		if(!!$container)
+		{
+			$return['success'] = true;
+			$return['data'] = $container->coordinates;
+		}
+
+		return $return;
 	}
 
-	public function create()
+	public function create($name, Request $request)
 	{
-		
+		if( !$request->has('longitude') || !$request->has('latitude') )
+		{
+			return ['success' => false, 'error' => 'No name longitude and/or latitude.'];
+		}
+
+		$container = Container::where('name', $name)->first();
+
+		if( !$container )
+		{
+			return ['success' => false, 'error' => 'Container does not exist']
+		}
+
+		return ['success' => true, 'data' => Coordinate::create(
+			array_merge(['container_id' => $container->id], $request->only(['longitude', 'latitude']))
+		)];
 	}
 
-	public function update($id)
+	public function update($id, Request $request)
 	{
+		if( !$request->has('longitude') || !$request->has('latitude') )
+		{
+			return ['success' => false, 'error' => 'No name longitude and/or latitude.'];
+		}
 
+		return ['success' => true, 'data' => Coordinate::find($id)->update($request->only(['longitude', 'latitude']))];
 	}
 
 	public function destroy($id)
 	{
+		$coordinate = Coordinate::find($id)->delete();
 
+		return ['success' => $coordinate];
 	}
-
 }
